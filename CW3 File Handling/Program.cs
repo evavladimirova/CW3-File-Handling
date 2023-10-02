515c8c4acacb20cc7eff530330cc09f06e0a68b6
sport <sport@LAPTOP-61I2ACB0>
2023-10-02 12:06:31 +0300

    Add project files.

---
+++ CW3 File Handling/Program.cs
@@ -0,0 +1,42 @@
+﻿class Example
+{
+    public static void Main()
+    {
+        DirectoryInfo currDir = new DirectoryInfo(@"C:\Users\sport\Documents\ACS\Others\0210_EvaV");
+
+        currDir.Create();
+
+        string path = @"C:\Users\sport\Documents\ACS\Others\0210_EvaV\old.txt";
+        string path2 = @"C:\Users\sport\Documents\ACS\Others\0210_EvaV\new.txt";
+
+
+        try
+        {
+            if (!File.Exists(path))
+            {
+                using (FileStream fs = File.Create(path)) { }
+            }
+
+            if (File.Exists(path2))
+            {
+                File.Delete(path2);
+            }
+
+            File.Move(path, path2);
+            Console.WriteLine($"File '{path}' has been moved to '{path2}'");
+
+            if (File.Exists(path))
+            {
+                Console.WriteLine("The original file exists.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+
+    }
+
+}
+
+
